8f45a4a55d4c2e526d2737e8b44b3b8fd1aff72f
Javier Sánchez Rois <jsanchez@gradiant.org>
2016-03-03 16:31:31 +0100

    Added templated ERB instead of harcoded file template

--- src/gmock_serializer.rb
+++ src/gmock_serializer.rb
@@ -11,33 +11,8 @@ class GMockSerializer
     sentence
 	end
   def get_template()
-%{#include <gmock/gmock.h>
-#include <%= test_suite.file %>
-
-class TestSuite : public ::testing::Test
-{
-protected:
- <%= test_suite.target_class + " object;" %>	
-};
-
-<% test_suite.test_cases.each do |test_case| %>
-TEST_F(TestSuite, <%=test_case.name%>)
-{
-<% test_case.preconditions.each do |precondition| %>
- <%= serialize_precondition precondition %>
-<% end %>
-<% test_case.expectations.each do |expectation| %>
- <%= serialize_expectation expectation %>
-<% end %>
-}
-<% end %>
-
-int main(int argc, char ** argv)
-{
-    ::testing::InitGoogleMock(&argc,argv);
-    return RUN_ALL_TESTS();
-}
-}
+  	template_name = File.join(File.dirname(__FILE__), "template.erb")
+		File.read(template_name)
   end
 
   def run (test_suite)
