51a2cabe2207750110d3d1937488aceea20e7a94
Javier Sánchez Rois <jsanchez@gradiant.org>
2016-03-03 09:42:52 +0100

    Generating tests with correct name

--- src/gmock_serializer.rb
+++ src/gmock_serializer.rb
@@ -3,13 +3,14 @@ class GMockSerializer
   def get_template()
     %{#include <gmock/gmock.h>
 #include <%= test_suite.file %>
+
 <% test_suite.test_cases.each do |test_case| %>
-TEST_F(TestSuite,<%=test_case.name%>){
-  <% test_case.sentences.each do |sentence| %>
-  <%= sentence.serialize %>
-  <% end %>
+TEST_F(TestSuite, <%=test_case.name%>)
+{
+ EXPECT_EQ(true,true);
 }
 <% end %>
+
     }
   end
 
--- src/test_generator.rb
+++ src/test_generator.rb
@@ -4,9 +4,10 @@ require 'gmock_serializer'
 require 'erb'
 
 if ARGV[0] then
-  collector = Collector.new
+  collector  = Collector.new
   test_suite = collector.scan_file ARGV[0]
   serializer = GMockSerializer.new
-  p serializer.run test_suite
+  test_code  = serializer.run test_suite
+  File.open('test.cpp', 'w') { |file| file.write(test_code) }
 end
 
